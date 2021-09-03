3058dfec0373e97375f06d6aac9b2909dd43000d
RitikaT19 <ritika.tiwari@headstrait.com>
2021-09-03 19:22:16 +0530

    feat: basics

---
+++ proj/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'question.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+
+    print(_questionIndex);
+  }
+  @override
+  Widget build(BuildContext context) {
+    @override
+    var questions = [
+      'What is your favourite colour?',
+      'What is your favourite animal?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My App'),
+        ),
+        body: Column(
+          children: [
+            Question(questions[_questionIndex]),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: _answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Answer 2'),
+              onPressed: _answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Answer 3'),
+              onPressed: _answerQuestion,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
