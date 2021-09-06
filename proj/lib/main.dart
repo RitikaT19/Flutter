16c080278605a00d743f2fd19c7817881036699a
RitikaT19 <ritika.tiwari@headstrait.com>
2021-09-06 17:47:52 +0530

    Feat: applied ternary operator prevent error after the last option

--- proj/lib/main.dart
+++ proj/lib/main.dart
@@ -13,8 +13,29 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final questions = const [
+    //Can also be created as Map()
+    {
+      'questionText': 'What is your favourite colour?',
+      'answer': ['Black', 'Red', 'Green', 'White'],
+    },
+    {
+      'questionText': 'What is your favourite animal?',
+      'answer': ['Lion', 'Dog', 'Elephant', 'Rabbit'],
+    },
+    {
+      'questionText': 'Which is your favourite place?',
+      'answer': ['New Delhi', 'Kolkata', 'Kashmir', 'Goa'],
+    }
+  ];
   var _questionIndex = 0;
+
   void _answerQuestion() {
+    if (_questionIndex < questions.length) {
+      print('we have more questions!');
+    } else {
+      print('No more questions!');
+    }
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -24,36 +45,25 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    @override
-    var questions = [
-      //Can also be created as Map()
-      {
-        'questionText': 'What is your favourite colour?',
-        "answer": ['Black', 'Red', 'Green', 'White'],
-      },
-      {
-        'questionText': 'What is your favourite animal?',
-        'answer': ['Lion', 'Dog', 'Elephant', 'Rabbit'],
-      },
-      {
-        'questionText': 'Which is your favourite place?',
-        'answer': ['New Delhi', 'Kolkata', 'Kashmir', 'Goa'],
-      }
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My App'),
         ),
-        body: Column(
-          children: [
-            Question(questions[_questionIndex]['questionText'].toString()),
-            ...(questions[_questionIndex]['answer']as List<String>).map((answer){
-              return Answer(_answerQuestion, answer);
-            }).toList() 
-    
-          ],
-        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: [
+                  Question(
+                      questions[_questionIndex]['questionText'].toString()),
+                  ...(questions[_questionIndex]['answer'] as List<String>)
+                      .map((answer) {
+                    return Answer(_answerQuestion, answer);
+                  }).toList()
+                ],
+              )
+            : Center(
+                child: Text('You did it!!'),
+              ),
       ),
     );
   }
